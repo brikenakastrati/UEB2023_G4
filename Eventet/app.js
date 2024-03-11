1851ee8c991696ee4ce7d7221de15be9c46fa173
Elonë <149248093+elonekrasniqi@users.noreply.github.com>
2024-03-11 18:16:31 +0100

    update

---
+++ Eventet/app.js
@@ -0,0 +1,213 @@
+
+function showWarning() {
+  alert("Ky button rinis Slideshow-in nga foto e parë !"); // Paraqet një vërejtje në një dialog alert
+}
+
+function scrollToSection(ProgrametDitore) {
+  const section = document.querySelector(ProgrametDitore);
+  if (section) {
+      section.scrollIntoView({
+          behavior: 'smooth'
+      });
+    }}
+
+function scrollToSection(NgjarjetPerseritura) {
+  const section = document.querySelector(NgjarjetPerseritura);
+  if (section) {
+      section.scrollIntoView({
+          behavior: 'smooth'
+      });
+  }
+}
+
+function scrollToSection(NgjarjetSpeciale) {
+  const section = document.querySelector(NgjarjetSpeciale);
+  if (section) {
+      section.scrollIntoView({
+          behavior: 'smooth'
+      });
+  }
+}
+function scrollToSection(NgjarjetKaluara) {
+  const section = document.querySelector(NgjarjetKaluara);
+  if (section) {
+      section.scrollIntoView({
+          behavior: 'smooth'
+      });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  try {
+    const cards = document.querySelectorAll('.card');
+
+    if (cards.length === 0) {
+      throw new Error('No card elements found.');
+    }
+
+    let index = 0;
+    while (index < cards.length) {
+      const card = cards[index];
+
+      card.addEventListener('mouseover', function() {
+        this.style.backgroundColor = 'rgba(25, 47, 87, 0.592)';
+      });
+
+      card.addEventListener('mouseout', function() {
+        this.style.backgroundColor = '';
+      });
+
+      index++;
+    }
+  } catch (error) {
+    console.error('An error occurred:', error.message);
+  }
+});
+
+function showAdditionalQuestions() {
+  const firstName = document.getElementById('firstName').value.trim();
+  const lastName = document.getElementById('lastName').value.trim();
+  const email = document.getElementById('email').value.trim();
+
+  const nameForm = document.getElementById('nameSurnameForm');
+  const additionalQuestionsForm = document.getElementById('myForm');
+  const timerDisplay = document.getElementById('timer');
+
+  if (isNaN(firstName) || isNaN(lastName)) {
+    // Set up the timer when the second form is displayed
+    let timeInSeconds = 120;
+    const timerInterval = setInterval(decrementTimer, 1000);
+
+    function updateTimer() {
+      const minutes = Math.floor(timeInSeconds / 60);
+      const seconds = timeInSeconds % 60;
+      const displayTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+      timerDisplay.innerText = displayTime;
+    }
+
+    function decrementTimer() {
+      if (timeInSeconds > 0) {
+        timeInSeconds--;
+        updateTimer();
+      } else {
+        clearInterval(timerInterval);
+        showResults();
+      }
+    }
+
+    setTimeout(() => {
+      clearInterval(timerInterval);
+      showResults(); // Call showResults when the timer reaches 2 minutes
+    }, 120000); // 2 minutes in milliseconds
+
+    nameForm.style.display = 'none';
+    additionalQuestionsForm.style.display = 'block';
+    timerDisplay.style.display = 'block'; // Show the timer
+  } else {
+    alert("Të lutem sheno emrin dhe mbiemrin nëse dëshironi të vazhdoni tutje! (Duke mos perdorur numra)");
+  }
+}
+
+
+function showResults() {
+  const correctAnswers = {
+    'planeti': 'planetiToka',
+    'galaktika': 'elliptike',
+    'komete': 'kometeB',
+    'yll': 'yllC',
+    'Sistemi': 'SD-C',
+    'planets': ['planetA', 'planetC']
+  };
+
+  const submittedAnswers = {
+    'planeti': $("input[name='planeti']:checked").val(),
+    'galaktika': $("input[name='galaktika']:checked").val(),
+    'komete': $("input[name='komete']:checked").val(),
+    'yll': $("input[name='yll']:checked").val(),
+    'Sistemi': $("input[name='Sistemi']:checked").val(),
+    'planets': [
+      $("#planetA").prop("checked") ? 'planetA' : '',
+      $("#planetC").prop("checked") ? 'planetC' : '',
+      $("#planetB").prop("checked") ? 'planetB' : ''
+    ]
+  };
+
+  const nameForm = document.getElementById('nameSurnameForm');
+  const additionalQuestionsForm = document.getElementById('myForm');
+  const timerDisplay = document.getElementById('timer');
+
+  nameForm.style.display = 'block';
+  additionalQuestionsForm.style.display = 'none';
+  timerDisplay.style.display = 'none';
+  
+  nameForm.reset();
+  additionalQuestionsForm.reset();
+
+  const resultsContainer = document.getElementById('rezultati');
+  resultsContainer.scrollIntoView();
+
+  let correctCount = 0;
+  let index = 1;
+  let resultHTML = "<strong>Rezultati juaj :</strong><br>";
+
+  for (let question in correctAnswers) {
+    if (question === 'planets') {
+      const submitted = submittedAnswers['planets'].filter(Boolean);
+      const correct = correctAnswers['planets'];
+
+      if (submitted.length === correct.length && correct.every((val) => submitted.includes(val))) {
+        resultHTML += `Pyetja ${index} është e saktë<br>`;
+        correctCount++;
+      } else {
+        resultHTML += `Pyetja ${index} është e pasaktë<br>`;
+      }
+    } else {
+      if (submittedAnswers[question] === correctAnswers[question]) {
+        resultHTML += `Pyetja ${index} është e saktë<br>`;
+        correctCount++;
+      } else {
+        resultHTML += `Pyetja ${index} është e pasaktë<br>`;
+      }
+    }
+    index++;
+  }
+
+  resultHTML += `<br>Numri i pergjigjjeve të sakta: ${correctCount}`;
+  $("#rezultati").html(resultHTML);
+
+  $("#learnMoreContainer").html('<br><p>Nese deshironi te dini me shume rreth astronomise, klikoni ketu:</p><br><button id="learnMoreButton" class="styledButton">Lexo me shume</button>');
+
+  $("#learnMoreButton").on("click", function () {
+    window.location.href = 'astronomy_page.html';
+  });
+}
+
+function toggleForm() {
+  var forma1 = $('#nameSurnameForm');
+  if (forma1.is(':hidden')) {
+    forma1.show();
+  } else {
+    forma1.hide();
+  }
+}
+
+$(document).ready(function() {
+
+  $(".box1, .box2").css("opacity", 1);
+
+    $(".box1, .box2").hover(function() {
+        $(this).stop().animate({
+            opacity: 0,
+        }, 3000);
+    }, function() {
+        $(this).stop().animate({
+            opacity: 1,
+        }, 3000);
+    });
+
+  $("dl dt, dl dd").hover(function() {
+      $(this).css("color", "#ff6600");
+  }, function() {
+      $(this).css("color", ""); 
+  });
+});
---
+++ Fakte/Faktepage.js
@@ -0,0 +1,98 @@
+$(document).ready(function () {
+
+    function createMeteor() {
+        const meteor = document.createElementNS("http://www.w3.org/2000/svg", "image");
+        meteor.setAttribute("class", "meteor");
+        meteor.setAttribute("width", "20");
+        meteor.setAttribute("height", "20");
+        meteor.setAttribute("x", Math.random() * window.innerWidth);
+        meteor.setAttribute("y", 0);
+        meteor.setAttribute("href", "metplease.png");
+        $('#meteors-container').append(meteor);
+        animateMeteor(meteor);
+    }
+
+
+    function animateMeteor(meteor) {
+        const speed = 2 + Math.random() * 3;
+        const angle = (5 / 6) * Math.PI;
+        const deltaX = Math.cos(angle) * speed;
+        const deltaY = Math.sin(angle) * speed;
+
+        function move() {
+            const currentX = parseFloat(meteor.getAttribute("x"));
+            const currentY = parseFloat(meteor.getAttribute("y"));
+
+            meteor.setAttribute("x", currentX + deltaX);
+            meteor.setAttribute("y", currentY + deltaY);
+
+            if (currentY < window.innerHeight) {
+                requestAnimationFrame(move);
+            } else {
+                meteor.remove();
+            }
+        }
+
+        requestAnimationFrame(move);
+    }
+
+
+    setInterval(createMeteor, 1000);
+});
+function butoniFazat() {
+    var div = document.getElementById('paragrafHena');
+    if (div.style.display === 'none') {
+        div.style.display = 'block';
+    } else {
+        div.style.display = 'none';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.querySelector('header');
+
+    function toggleHeaderBackground() {
+        header.classList.toggle('scrolled', window.scrollY > 50);
+    }
+
+    window.addEventListener('scroll', toggleHeaderBackground);
+});
+
+$(document).ready(function () {
+    function generateMeteor() {
+        const meteorElement = document.createElementNS("http://www.w3.org/2000/svg", "image");
+        meteorElement.setAttribute("class", "meteor");
+        meteorElement.setAttribute("width", "20");
+        meteorElement.setAttribute("height", "20");
+        meteorElement.setAttribute("x", Math.random() * window.innerWidth);
+        meteorElement.setAttribute("y", 0);
+        meteorElement.setAttribute("href", "metplease.png");
+        $('#meteors-container3').append(meteorElement);
+        moveMeteor(meteorElement);
+    }
+
+    function moveMeteor(meteorElement) {
+        const speed = 2 + Math.random() * 3;
+        const angle = (5 / 6) * Math.PI;
+        const deltaX = Math.cos(angle) * speed;
+        const deltaY = Math.sin(angle) * speed;
+
+        function move() {
+            const currentX = parseFloat(meteorElement.getAttribute("x"));
+            const currentY = parseFloat(meteorElement.getAttribute("y"));
+
+            meteorElement.setAttribute("x", currentX + deltaX);
+            meteorElement.setAttribute("y", currentY + deltaY);
+
+            if (currentY < window.innerHeight) {
+                requestAnimationFrame(move);
+            } else {
+                meteorElement.remove();
+            }
+        }
+
+        requestAnimationFrame(move);
+    }
+
+    setInterval(generateMeteor, 1000);
+});
